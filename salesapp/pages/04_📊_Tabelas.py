97cdc7757e1e36ca073ae456d3384a6949e7aa90
Alex Cordeiro <alexcordeiro58@gmail.com>
2026-07-01 11:18:14 -0300

    configurado a página Tabelas

--- salesapp/pages/04_📊_Tabelas.py
+++ salesapp/pages/04_📊_Tabelas.py
@@ -9,6 +9,43 @@ leitura_tabelas()
 df_vendas = st.session_state['dados']['df_vendas']
 df_filiais= st.session_state['dados']['df_filiais']
 df_produtos = st.session_state['dados']['df_produtos']
-df_vendas
-df_filiais
-df_produtos
+
+def mostra_tabela_produtos():
+    st.markdown('# Tabela de Produtos')
+    st.dataframe(df_produtos)
+
+def mostra_tabela_filiais():
+    st.markdown('# Tabela de Filiais')
+    st.dataframe(df_filiais)
+
+def mostra_tabela_vendas():
+    st.markdown('# Tabela de Vendas')
+    st.sidebar.divider()
+    st.sidebar.markdown('## Filtrar Tabela')
+    colunas_selecionadas = st.sidebar.multiselect('Selecione as colunas da Tabela:', 
+                                                  list(df_vendas.columns),list(df_vendas.columns))
+    col1,col2 = st.sidebar.columns(2)
+    filtro_selecionado = col1.selectbox('Filtrar coluna', list(df_vendas.columns))
+    valor_unico_selecionado = list(df_vendas[filtro_selecionado].unique())
+    valor_selecionado = col2.selectbox('Valor do filtro', valor_unico_selecionado)
+    filtrar = col1.button('Filtrar')
+    limpar = col2.button('Limpar')
+
+    if filtrar:
+        st.dataframe(df_vendas.loc[df_vendas[filtro_selecionado] == valor_selecionado, colunas_selecionadas],height = 800)
+    elif limpar:
+       st.dataframe(df_vendas[colunas_selecionadas],height = 800)
+    else:
+       st.dataframe(df_vendas[colunas_selecionadas],height = 800)
+       
+
+st.sidebar.markdown('**Seleção de Tabelas**')
+opcao_tabelas = st.sidebar.selectbox('Selecione a tabela que você deseja ver:',['Vendas', 'Filiais', 'Produtos'])
+
+if opcao_tabelas == 'Produtos':
+    mostra_tabela_produtos()
+elif opcao_tabelas == 'Vendas':
+    mostra_tabela_vendas()
+elif opcao_tabelas == 'Filiais':
+    mostra_tabela_filiais()
+
--- salesapp/pages/05_📈📉_Adicao_e_Remocao_de_Vendas.py
+++ salesapp/pages/05_📈📉_Adicao_e_Remocao_de_Vendas.py
@@ -9,6 +9,13 @@ leitura_tabelas()
 df_vendas = st.session_state['dados']['df_vendas']
 df_filiais= st.session_state['dados']['df_filiais']
 df_produtos = st.session_state['dados']['df_produtos']
+
+
+
+
+
+
+
 df_vendas
 df_filiais
 df_produtos
